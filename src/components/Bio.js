699a2bfa09b75984f1da18bf1a0e40f65b37f0e2
arieldavis22 <aadavisbusiness@gmail.com>
2020-06-28 22:24:04 -0400

    test animation;

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image } from 'semantic-ui-react'
 import '../css/Bio.css'
-import { Canvas } from 'react-three-fiber'
-import Box from './Box'
+// import { Canvas } from 'react-three-fiber'
+// import Box from './Box'
+import { useSpring, animated } from 'react-spring';
 
 const Bio = () => {
+
+    const [on, setOn] = useState(false)
+
+    const { xy, c } = useSpring({
+        from: { xy: [0, 0], c: 'green' },
+        xy: on ? [800, 200] : [0, 0],
+        c: on ? 'red' : 'green'
+    })
+
+    const toggle = () => {
+        setOn(!on)
+    }
 
     return (  
         <div className="bio container">
@@ -15,20 +28,23 @@ const Bio = () => {
             circular/>
             <div>
                 {/* main content of my bio */}
-                <p>
+                <animated.p 
+                style={
+                    {transform: xy.interpolate((x, y) => `translate(${x}px, ${y}px)`),
+                    color: c.interpolate(c => c)}} 
+                    onClick={toggle}>
                     Full stack web developer with a passion for expansive learning. <br/> 
                     Experience in Ruby on Rails, JavaScript, and React tied to robotics and programming <br/>
                     via work with the Technology Student Association. Deeply invested in problem solving, <br/>
                     user experience and possess strong skills in team-building, deadline management, and <br/>
                     communications that now fluently transfer into work as a developer.
-                </p>
+                </animated.p>
             </div>
-            <Canvas colorManagement>
+            {/* <Canvas colorManagement>
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
-            </Canvas>
+                <Box position={[0, 0, 0]} />
+            </Canvas> */}
         </div>
     );
 }
--- src/components/Box.js
+++ src/components/Box.js
@@ -13,6 +13,7 @@ const Box = (props) => {
     useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
 
     return (
+      <>
         <mesh
             {...props}
             ref={mesh}
@@ -23,6 +24,7 @@ const Box = (props) => {
             <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
             <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
         </mesh>
+      </>
     )
 }
 
